f5d13729b9d8274f300591673ae06f309646daeb
Mrunal Khanke <136327297+Mrunalkhanke@users.noreply.github.com>
2024-04-11 14:55:00 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3><b>Quick links</b></h3>
+         <a href="home.php"><b>Home</b></a>
+         <a href="about.php"><b>About</b></a>
+         <a href="community.php"><b>Community</b></a>
+         <a href="quiz.php"><b>Quiz</b></a>
+         <a href="report.php"><b>Report</b></a>
+         <a href="contact.php"><b>Contact</b></a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php"><b>Login</b></a>
+         <a href="register.php"><b>Register</b></a>
+         
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i>  </p>
+         
+         <p> <i class="fas fa-envelope"></i>  </p>
+         <p> <i class="fas fa-map-marker-alt"></i> </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+
+</section>
---
+++ header.php
@@ -0,0 +1,54 @@
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+
+<header class="header">
+
+   <div class="header-1">
+      <div class="flex">
+         <div class="share">
+         </div>
+         <p> <a href="login.php">Login</a> | <a href="Register.php">register</a> </p>
+      </div>
+   </div>
+
+   <div class="header-2">
+      <div class="flex">
+         <a href="home.php" class="logo"><b>FinTech.</b></a>
+
+         <nav class="navbar">
+            <a href="home.php"><b>Home</b></a>
+            <a href="about.php"><b>About</b></a>
+            <a href="community.php"><b>Community</b></a>
+            <a href="quiz.php"><b>Quiz</b></a>
+            <a href="report.php"><b>Report</b></a>
+          
+         </nav>
+
+         <div class="icons">
+            <div id="user-btn" class="fas fa-user"></div>
+            <?php
+               $select_cart_number = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+               $cart_rows_number = mysqli_num_rows($select_cart_number); 
+            ?>
+            
+         </div>
+
+         <div class="user-box">
+            <p>username : <span><?php echo $_SESSION['user_name']; ?></span></p>
+            <p>email : <span><?php echo $_SESSION['user_email']; ?></span></p>
+            <a href="logout.php" class="delete-btn">logout</a>
+         </div>
+      </div>
+   </div>
+
+</header>
